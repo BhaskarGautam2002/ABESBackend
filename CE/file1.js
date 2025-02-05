f53684e54b341d4c8b3391eff77c71c11c8821ad
Bhaskar <bhaskargautam422@gmail.com>
2025-02-05 09:57:19 +0530

    5/2/25 work2

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,13 @@
-const fs = require('fs'); //require(node:fs);
-const data = fs.readFileSync("./data.txt", "utf-8" );// const data = fs.readFileSync("./data.txt");// fs.readFileSync("./data.txt");
+const { Console } = require('console');
+const fs = require('fs'); //require(node:"fs");
+const data = fs.readFileSync("./data.txt", "utf-8" );// const data = fs.readFileSync("./data.txt", encoding:"utf-8" );// const data = fs.readFileSync("./data.txt");// fs.readFileSync("./data.txt");
 console.log(data);// console.log(data.toString());// console.log(data);
+// fs.writeFileSync("./data1.txt", "ABES promise");
+// fs.appendFileSync("./data1.txt", "AKTU promise");
+// fs.renameSync("./data1.txt", "./data2.txt");
+// fs.unlinkSync("./data2.txt");
+if (data.match("H")) {
+    console.log("H is present");
+    const changedata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", changedata);
+}
